86b5822fc496b9441cf52b163ca42a98be6a625f
Ashish <73551845+sonic-sabers@users.noreply.github.com>
2024-02-02 20:51:06 +0530

    Delete node_modules directory

--- node_modules/@tamagui/helpers/types/index.d.ts
+++
@@ -1,8 +0,0 @@
-export * from '@tamagui/simple-hash';
-export * from './clamp';
-export * from './composeEventHandlers';
-export * from './concatClassName';
-export * from './types';
-export * from './validStyleProps';
-export * from './withStaticProperties';
-//# sourceMappingURL=index.d.ts.map
