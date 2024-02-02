23fdd9b9a63f817985896ed2e21640272134e2ba
Ashish <73551845+sonic-sabers@users.noreply.github.com>
2024-02-02 20:51:53 +0530

    Delete node_modules directory

--- node_modules/@tamagui/create-theme/src/index.tsx
+++
@@ -1,6 +0,0 @@
-export * from './createTheme'
-export * from './masks'
-export * from './combineMasks'
-export * from './applyMask'
-export * from './createThemeTypes'
-export * from './helpers'
--- node_modules/@tamagui/web/src/hooks/useDisableSSR.tsx
+++
@@ -1,6 +0,0 @@
-import { getConfig } from '../config'
-import { ComponentContextI } from '../types'
-
-export function getDisableSSR(componentContext?: ComponentContextI) {
-  return componentContext?.disableSSR ?? getConfig().disableSSR
-}
